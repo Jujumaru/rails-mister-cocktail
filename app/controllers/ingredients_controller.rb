9478335b8c4f299d7ead2497d5918edfea26cb01
Julien Bidart <jbidart@ncsu.edu>
2020-05-15 16:07:40 +0200

    finally kinda working

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,19 +1,39 @@
-lass IngredientsController < ApplicationController
-  def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.new(ingredient_params)
-    @ingredient.cocktail = @cocktail
-    if @ingredient.save
-      redirect_to cocktail_path(@cocktail)
-    else
-      @dose = Dose.new
-      render "cocktails/show"
+class IngredientsController < ApplicationController
+    def index
+      @ingredients = Ingredient.all
     end
-  end
-
-  private
-
-  def ingredient_params
-    params.require(:ingredient).permit(:rating, :content)
-  end
+  
+    def show
+      @ingredient = Ingredient.find(params[:id])
+    end
+  
+    def new
+      @ingredient = Ingredient.new
+    end
+  
+    def create
+      @cocktail = Cocktail.find(params[:id])
+      @ingredient = Ingredient.new(ingredient_params)
+      @ingredient.cocktail = @cocktail
+      @ingredient.save
+      redirect_to ingredient_path(@ingredient)
+    end
+  
+    def edit
+      @ingredient.update(ingredient_params)
+      @ingredient = Ingredient.find(params[:id])
+      redirect_to ingredient_path(@ingredient)
+    end
+  
+    def destroy
+      @ingredient = Ingredient.find(params[:id])
+      @ingredient.destroy
+      redirect_to ingredients_path
+    end
+  
+    private
+  
+    def ingredient_params
+      params.require(:ingredient).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :cocktails, :ingredients do
-    resources :doses
-  end
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ resources :cocktails, only: [ :index, :show, :new, :create] do
+   resources :doses, only: [ :new, :create, :show, :destroy ]
+ end
 end
